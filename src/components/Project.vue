d73600712c4907c833842b1dec542bdee54a2cac
elarsaks <elarsaks@gmail.com>
2021-03-09 13:00:06 +0200

    Add cursor:pointer to project.

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,57 +1,53 @@
 <template>
   <div class="project">
-      <img 
-        class="project-img" 
-        :src="project.image"
-        @click="$emit('show-project', project.name)" 
-      />
+    <img
+      class="project-img"
+      :src="project.image"
+      @click="$emit('show-project', project.name)"
+    />
 
-      <transition name="slide">
-        <div
-          v-if="projectOpen == project.name"
-          class="project-info"
-        >
-          <h3> {{ project.name }} </h3>
-          <h4> {{ project.role }} </h4>
-          <h4 class="time"> {{ project.time }} </h4>
-          <h4 class="in-text">Technologies: </h4>
-          <p> {{project.technologies}} </p>
+    <transition name="slide">
+      <div v-if="projectOpen == project.name" class="project-info">
+        <h3>{{ project.name }}</h3>
+        <h4>{{ project.role }}</h4>
+        <h4 class="time">{{ project.time }}</h4>
+        <h4 class="in-text">Technologies:</h4>
+        <p>{{ project.technologies }}</p>
 
-          <h4 class="in-text">Description: </h4>
-          <p v-for="p in project.description" v-bind:key="p" > {{ p }} </p>
+        <h4 class="in-text">Description:</h4>
+        <p v-for="p in project.description" v-bind:key="p">{{ p }}</p>
 
-          <h4 class="in-text">Links: </h4>
-          <div v-for="link in project.links" v-bind:key="link.link">
-            <p class="links">
-              {{ link.text }}
-              <br v-if="windowWith < 1024" >
-              <a @click="openLink(link.link)">{{ link.link }}</a>
-            </p>
-          </div>
+        <h4 class="in-text">Links:</h4>
+        <div v-for="link in project.links" v-bind:key="link.link">
+          <p class="links">
+            {{ link.text }}
+            <br v-if="windowWith < 1024" />
+            <a @click="openLink(link.link)">{{ link.link }}</a>
+          </p>
+        </div>
       </div>
-      </transition>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['project', 'projectOpen'],
+  props: ["project", "projectOpen"],
   methods: {
-    openLink(adress){
-      console.log(adress)
-      window.open(adress, "_blank")
-    },
+    openLink(adress) {
+      console.log(adress);
+      window.open(adress, "_blank");
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-
 a {
   text-decoration: underline;
   color: blue;
 }
-  
+
 h3 {
   padding: 0;
   margin: 0;
@@ -77,6 +73,7 @@ p {
 
 .project {
   text-align: center;
+  cursor: pointer;
 }
 
 .project-info {
@@ -102,33 +99,33 @@ p {
 }
 
 .slide-enter-active {
-   -moz-transition-duration: 0.3s;
-   -webkit-transition-duration: 0.3s;
-   -o-transition-duration: 0.3s;
-   transition-duration: 0.3s;
-   -moz-transition-timing-function: ease-in;
-   -webkit-transition-timing-function: ease-in;
-   -o-transition-timing-function: ease-in;
-   transition-timing-function: ease-in;
+  -moz-transition-duration: 0.3s;
+  -webkit-transition-duration: 0.3s;
+  -o-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -moz-transition-timing-function: ease-in;
+  -webkit-transition-timing-function: ease-in;
+  -o-transition-timing-function: ease-in;
+  transition-timing-function: ease-in;
 }
 
 .slide-leave-active {
-   -moz-transition-duration: 0.3s;
-   -webkit-transition-duration: 0.3s;
-   -o-transition-duration: 0.3s;
-   transition-duration: 0.3s;
-   -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
-   -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
-   -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
-   transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -moz-transition-duration: 0.3s;
+  -webkit-transition-duration: 0.3s;
+  -o-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
 }
 
-.slide-enter-to, .slide-leave {
-   overflow: hidden;
+.slide-enter-to,
+.slide-leave {
+  overflow: hidden;
 }
 
-@media (min-width:1023px) {
-
+@media (min-width: 1023px) {
   .project {
     padding-bottom: 0;
     padding-top: 5vh;
